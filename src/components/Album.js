632c96e4d23ffecb6ddc38017061dac6fcba10b7
lkraman <laurakraman01@gmail.com>
2019-01-21 14:43:17 -0500

    checkpoint-bloc-jams-react-player-bar-range-inputs completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component {
   constructor(props) {
@@ -62,6 +63,13 @@ class Album extends Component {
       } else return <span>{index + 1}</span>;
     };
 
+    handlePrevClick() {
+       const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+       const newIndex = Math.max(0, currentIndex - 1);
+       const newSong = this.state.album.songs[newIndex];
+       this.setSong(newSong);
+       this.play();
+    }
 
   render() {
     return (
@@ -95,6 +103,12 @@ class Album extends Component {
           ))}
           </tbody>
         </table>
+        <PlayerBar
+        isPlaying={this.state.isPlaying}
+        currentSong={this.state.currentSong}
+        handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+        handlePrevClick={() => this.handlePrevClick()}
+        />
      </section>
    )
  }
