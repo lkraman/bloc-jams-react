a3f6bea5c358d0c748e2036b44ed9c61f9a0431e
lkraman <laurakraman01@gmail.com>
2019-02-23 07:41:11 -0500

    commit changes 2.23.2019 version 3

--- src/components/Library.js
+++ src/components/Library.js
@@ -12,16 +12,26 @@ class Library extends Component {
   render() {
     return (
       <section className='library'>
+      <div className="library-wrapper" id="library-wrapper">
+      <div className="library-container" id="library-container">
         {
           this.state.albums.map( (album, index) =>
             <Link to={`/album/${album.slug}`} key={index}>
+            <div className="photo-grid clearfix">
+            <ul className="clearfix">
+            <li>
               <img src={album.albumCover} alt={album.title} />
               <div>{album.title}</div>
               <div>{album.artist}</div>
               <div>{album.songs.length} songs</div>
+              </li>
+              </ul>
+               </div>
             </Link>
           )
         }
+      </div>
+      </div>
       </section>
     );
   }
